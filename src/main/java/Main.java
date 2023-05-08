b5b4be90cac1176d737bc725a2e95bb653945690
bbring2 <jennachoi27@gmail.com>
2023-05-08 23:34:18 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.util.Arrays;
 
 public class Main {
 
+    public static void main(String[] args) throws JsonProcessingException {
+        CategoryTree tree = new CategoryTree();
+        tree.addNode(1, "남자", 1);
+    }
 }
